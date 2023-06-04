7716b05fa8ba0e4ead56dc558527c65032b7c9f3
Emma Ladouceur <emmala@gmail.com>
2023-06-04 21:35:35 +0200

    restoration dataset

--- week_6/Restoration_Data.R
+++ week_6/Restoration_Data.R
@@ -2,29 +2,31 @@
 
 library(tidyverse)
 
-#Data found [here](https://datadryad.org/stash/dataset/doi:10.5061/dryad.rj849k6)
+# Data found [here](https://datadryad.org/stash/dataset/doi:10.5061/dryad.rj849k6)
 
  rest_data <- read.csv("~/Downloads/latdata.csv")
 
-
+# Let's explore the hierarchy of these data
  head(rest_data)
 
  # How many papers?
  rest_data %>% select(Citation) %>% distinct()
 
- #life forms
+ # Life forms
  rest_data %>% select(LifeFormBroad) %>% distinct()
 
- #Trophic levels
+ # Trophic levels
  rest_data %>% select(TrophLevel) %>% distinct()
 
  rest_data %>% select( LifeFormBroad, TrophLevel) %>% distinct()
 
-
+# response variables
  rest_data %>% select(RV) %>% distinct()
 
+ # disturbances, restoration activities and habitats
  rest_data %>% select(DisturbCat) %>% distinct()
  rest_data %>% select(DisturbCat, AllActivities, ActiveRestor, Control) %>% distinct()
+
 
  rest_data %>% select(HabitatCat) %>% distinct()
  rest_data %>% select(HabitatCat, DisturbCat) %>% distinct()
